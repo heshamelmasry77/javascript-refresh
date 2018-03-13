39c2dfc049f387f189bb279d94b47a8d79826549
Hesham El Masry <heshamelmasry@outlook.com>
2018-03-13 22:03:25 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function findBiggestFraction(a, b) {
+  console.log('test');
+  a > b ? console.log('a: ', a) : console.log('b: ', b);
+}
+
+var a = 3 / 4;
+var b = 5 / 5;
+
+findBiggestFraction(a, b);
